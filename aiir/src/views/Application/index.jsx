09ae3700cf6d93707b4e1c8d3946db45fc10cbb6
unknown <kamkucharsky@gmail.com>
2019-04-19 22:32:09 +0200

    Welcome

---
+++ aiir/src/views/Application/index.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import { withRouter } from "react-router-dom";
+
+class Application extends Component {
+  render() {
+    return <div>Application here</div>;
+  }
+}
+
+export default withRouter(Application);
---
+++ aiir/src/views/Welcome/style.js
@@ -0,0 +1,32 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  width: 100vw;
+  height: 100vh;
+  background-color: #e2dcdc;
+
+  display: grid;
+  justify-items: center;
+  align-items: center;
+  grid-template: "top" "mid" "bot" /1fr;
+`;
+
+export const Header = styled.div`
+  font-family: "Roboto:700i", sans-serif;
+  font-size: 1.5em;
+  position: relative;
+  text-align: center;
+  padding: 14px;
+  border-radius: 14px;
+  background-color: #c1bdbd;
+`;
+
+export const Button = styled.button`
+  background: transparent;
+  border-radius: 3px;
+  border: 2px solid palevioletred;
+  color: palevioletred;
+  margin: 0 1.5em;
+  padding: 0.25em 1em;
+  height: 2.5em;
+`;
